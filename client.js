2db44616b9815e539cdff02c12f70dbd95871fb4
Max Li <hope_max@outlook.com>
2021-08-14 23:27:04 -0400

    Add move command

--- client.js
+++ client.js
@@ -14,11 +14,26 @@ const connect = function() {
 
   conn.on('connect', () => {
     conn.write('Name: Max');
+    const moves = ['up', 'left', 'down', 'down', 'right'];
+    for (let i = 0; i < moves.length; i++) {
+      setTimeout(() => conn.write(`Move: ${moves[i]}`), 1000 + i * 100);
+    }
+    // setTimeout(() => conn.write('Move: up'), 1050);
+    // setTimeout(() => conn.write('Move: left'), 1100);
+    // setTimeout(() => conn.write('Move: down'), 1150);
+    // setTimeout(() => conn.write('Move: down'), 1200);
+
+    // setInterval((direction) => conn.write(`Move: ${direction}`), 1000, 'up');
   });
 
   conn.on('data', () => {
     console.log('Server says: you ded cuz you idled');
   });
+
+  // conn.on('data', () => {
+  //   process.exit();
+  // });
+
   return conn;
 };
 
