59fc7c0380b4f00a2520cae6e734de6a5696c149
Max Li <hope_max@outlook.com>
2021-08-14 20:34:41 -0400

    Refactored

--- play.js
+++ play.js
@@ -1,17 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-  conn.on('data', () => {
-    console.log('Server says: you ded cuz you idled');
-  });
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
