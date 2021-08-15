b8159b2f13f6407174d5c7f7d737b23cc138a480
Max Li <hope_max@outlook.com>
2021-08-14 22:21:40 -0400

    Updated

--- client.js
+++ client.js
@@ -7,6 +7,15 @@ const connect = function() {
   });
 
   conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: Max');
+  });
+
   conn.on('data', () => {
     console.log('Server says: you ded cuz you idled');
   });
